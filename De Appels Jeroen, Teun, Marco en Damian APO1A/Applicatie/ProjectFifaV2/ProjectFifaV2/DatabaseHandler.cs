d704c70c1d79f9e5bd16f5cdc6a17978e4d32d84
DlBreda <damian@marcleijten.nl>
2014-06-03 14:34:50 +0200

    Ingeleverd project backup

---
+++ De Appels Jeroen, Teun, Marco en Damian APO1A/Applicatie/ProjectFifaV2/ProjectFifaV2/DatabaseHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.SqlServerCe;
+using System.Windows.Forms;
+using System.Data;
+
+namespace ProjectFifaV2
+{
+    class DatabaseHandler
+    {
+        private SqlCeConnection con;
+
+        public DatabaseHandler()
+        {
+            con = new SqlCeConnection(@"Data Source=DB.sdf");
+        }
+
+        public void TestConnection()
+        {
+            bool open = false;
+            
+            try
+            {
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    open = true;
+                }
+                con.Close();
+            }
+
+            
+
+            if (!open)
+            {
+                Application.Exit();
+            }
+        }
+
+        public void OpenConnectionToDB()
+        {
+            con.Open();
+        }
+
+        public void CloseConnectionToDB()
+        {
+            con.Close();
+        }
+
+        public System.Data.DataTable FillDT(string query)
+        {
+            SqlCeDataAdapter dataAdapter = new SqlCeDataAdapter(query, GetCon());
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+
+            return dt;
+        }
+
+        public SqlCeConnection GetCon()
+        {
+            return con;
+        }
+    }
+}
---
+++ De Appels Jeroen, Teun, Marco en Damian APO1A/Applicatie/ProjectFifaV2/ProjectFifaV2/frmPlayer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProjectFifaV2
+{
+    public partial class frmPlayer : Form
+    {
+        private Form frmRanking;
+        private DatabaseHandler dbh;
+        private string userName;
+        private int userID;
+        private Form frmlogin;
+
+        const int minPoints = 0;
+        const int maxPoints = 50;
+        const string minusSign = "-";
+
+        //List<TextBox> txtBoxList;
+
+        public frmPlayer(Form frm, string un, Form frmlog, int userId)
+        {
+            this.ControlBox = false;
+            frmRanking = frm;
+            frmlogin = frmlog;
+            dbh = new DatabaseHandler();
+            userID = userId;
+
+            InitializeComponent();
+            if (DisableEditButton())
+            {
+                btnEditPrediction.Enabled = false;
+            }
+            ShowResults();
+            ShowScoreCard();
+            this.Text = "Welcome " + un;
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageHandler.ShowMessageWithResult("Are you sure you want to log out?", "Log out", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result.Equals(DialogResult.OK))
+            {
+                Hide();
+                frmlogin.BringToFront();
+            }         
+        }
+
+        private void btnShowRanking_Click(object sender, EventArgs e)
+        {
+            frmRanking = new frmRanking();
+            frmRanking.Show();
+        }
+
+        private void btnClearPrediction_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageHandler.ShowMessageWithResult("Are you sure you want to clear your prediction?", "Clear Predictions", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result.Equals(DialogResult.OK))
+            {
+                // Clear predections
+                foreach (Control control in pnlPredCard.Controls)
+                {
+                    NumericUpDown numControls = control as NumericUpDown;
+                    if (numControls != null)
+                    {
+                        numControls.Value = minPoints;
+                    }
+
+                }
+                MessageBox.Show("Er wordt wel gecleared behalve uit de database.");
+
+                // Update DB
+                dbh.TestConnection();
+                dbh.OpenConnectionToDB();
+
+                using (SqlCeCommand cmd = new SqlCeCommand("DELETE from [tblPredictions] WHERE User_ID = @UserID", dbh.GetCon()))
+                {
+                    cmd.Parameters.AddWithValue("UserID", userID);
+                    MessageHandler.ShowMessage(cmd.ExecuteNonQuery().ToString());
+                }
+
+                dbh.CloseConnectionToDB();
+            }
+        }
+
+        private bool DisableEditButton()
+        {
+            bool hasPassed;
+            //This is the deadline for filling in the predictions
+            DateTime deadline = new DateTime(2014, 06, 12);
+            DateTime curTime = DateTime.Now;
+            int result = DateTime.Compare(deadline, curTime);
+
+            if (result < 0)
+            {
+                hasPassed = true;
+            }
+            else
+            {
+                hasPassed = false;
+            }
+
+            return hasPassed;
+        }
+
+        private void ShowResults()
+        {
+            dbh.TestConnection();
+            dbh.OpenConnectionToDB();
+
+            DataTable hometable = dbh.FillDT("SELECT tblTeams.TeamName, tblGames.HomeTeamScore FROM tblGames INNER JOIN tblTeams ON tblGames.HomeTeam = tblTeams.Team_ID");
+            DataTable awayTable = dbh.FillDT("SELECT tblTeams.TeamName, tblGames.AwayTeamScore FROM tblGames INNER JOIN tblTeams ON tblGames.AwayTeam = tblTeams.Team_ID");
+
+            dbh.CloseConnectionToDB();
+
+            for (int i = 0; i < hometable.Rows.Count; i++)
+            {
+                DataRow dataRowHome = hometable.Rows[i];
+                DataRow dataRowAway = awayTable.Rows[i];
+                ListViewItem lstItem = new ListViewItem(dataRowHome["TeamName"].ToString());
+                lstItem.SubItems.Add(dataRowHome["HomeTeamScore"].ToString());
+                lstItem.SubItems.Add(minusSign);
+                lstItem.SubItems.Add(dataRowAway["AwayTeamScore"].ToString());
+                lstItem.SubItems.Add(dataRowAway["TeamName"].ToString());
+                lvOverview.Items.Add(lstItem);
+            }
+        }
+
+        private void ShowScoreCard()
+        {
+            dbh.TestConnection();
+            dbh.OpenConnectionToDB();
+
+            DataTable hometable = dbh.FillDT("SELECT tblTeams.TeamName FROM tblGames INNER JOIN tblTeams ON tblGames.HomeTeam = tblTeams.Team_ID");
+            DataTable awayTable = dbh.FillDT("SELECT tblTeams.TeamName FROM tblGames INNER JOIN tblTeams ON tblGames.AwayTeam = tblTeams.Team_ID");
+
+            dbh.CloseConnectionToDB();
+
+            for (int i = 0; i < hometable.Rows.Count; i++)
+            {
+                DataRow dataRowHome = hometable.Rows[i];
+                DataRow dataRowAway = awayTable.Rows[i];
+                
+                Label lblHomeTeam = new Label();
+                Label lblAwayTeam = new Label();
+                NumericUpDown numHomePred = new NumericUpDown();
+                NumericUpDown numAwayPred = new NumericUpDown();
+
+                lblHomeTeam.TextAlign = ContentAlignment.BottomRight;
+                lblHomeTeam.Text = dataRowHome["TeamName"].ToString();
+                lblHomeTeam.Location = new Point(15, numHomePred.Bottom + (i * 30));
+                lblHomeTeam.AutoSize = true;
+
+                numHomePred.Value = 0;
+                numHomePred.Minimum = minPoints;
+                numHomePred.Maximum = maxPoints;
+                numHomePred.Location = new Point(lblHomeTeam.Width, lblHomeTeam.Top - 3);
+                numHomePred.Width = 40;
+
+                numAwayPred.Value = 0;
+                numAwayPred.Minimum = minPoints;
+                numAwayPred.Maximum = maxPoints;
+                numAwayPred.Location = new Point(numHomePred.Width + lblHomeTeam.Width, numHomePred.Top);
+                numAwayPred.Width = 40;
+
+                lblAwayTeam.Text = dataRowAway["TeamName"].ToString();
+                lblAwayTeam.Location = new Point(numHomePred.Width + lblHomeTeam.Width + numAwayPred.Width, numHomePred.Top + 3);
+                lblAwayTeam.AutoSize = true;
+
+                pnlPredCard.Controls.Add(lblHomeTeam);
+                pnlPredCard.Controls.Add(numHomePred);
+                pnlPredCard.Controls.Add(numAwayPred);
+                pnlPredCard.Controls.Add(lblAwayTeam);
+                
+                //ListViewItem lstItem = new ListViewItem(dataRowHome["TeamName"].ToString());
+                //lstItem.SubItems.Add(dataRowHome["HomeTeamScore"].ToString());
+                //lstItem.SubItems.Add(dataRowAway["AwayTeamScore"].ToString());
+                //lstItem.SubItems.Add(dataRowAway["TeamName"].ToString());
+                //lvOverview.Items.Add(lstItem);
+            }
+        }
+        internal void GetUsername(string un)
+        {
+            userName = un;
+        }
+
+        private void btnEditPrediction_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Werkt niet...");
+        }
+    }
+}
